5d81318709a3dda8555c0d63a65f4250b424b232
Wężyk Kamil <kamil.wezyk@execon.pl>
2023-04-05 18:17:53 +0200

    updated to be compatible with  yoheinakajima

--- index.js
+++ index.js
@@ -1,136 +1,201 @@
-const { Configuration, OpenAIApi } = require("openai");
+const Pinecone = require("@pinecone-database/pinecone");
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
 
 // Set API Keys
-const OPENAI_API_KEY = "";
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+if (!OPENAI_API_KEY) throw new Error("OPENAI_API_KEY environment variable is missing from .env");
 
-// Set Variables
-const OBJECTIVE = "Solve world hunger.";
-const YOUR_FIRST_TASK = "Develop a task list.";
+const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
+if (!PINECONE_API_KEY) throw new Error("PINECONE_API_KEY environment variable is missing from .env");
+
+const PINECONE_ENVIRONMENT = process.env.PINECONE_ENVIRONMENT || "us-east1-gcp";
+const YOUR_TABLE_NAME = process.env.TABLE_NAME;
+if (!YOUR_TABLE_NAME) throw new Error("TABLE_NAME environment variable is missing from .env");
+
+const OBJECTIVE = process.argv[2] || process.env.OBJECTIVE;
+if (!OBJECTIVE) throw new Error("OBJECTIVE environment variable is missing from .env");
+
+const YOUR_FIRST_TASK = process.env.FIRST_TASK;
+if (!YOUR_FIRST_TASK) throw new Error("FIRST_TASK environment variable is missing from .env");
+
+const USE_GPT4 = process.env.USE_GPT4 || false;
+if (USE_GPT4) {
+    console.log('\x1b[91m%s\x1b[0m', "\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****");
+}
 
 // Print OBJECTIVE
-console.log("\x1b[96m\x1b[1m" + "\n*****OBJECTIVE*****\n" + "\x1b[0m\x1b[0m");
+console.log("\x1b[96m\x1b[1m*****OBJECTIVE*****\n\x1b[0m\x1b[0m");
 console.log(OBJECTIVE);
 
-// Configure OpenAI
-const configuration = new Configuration({apiKey: OPENAI_API_KEY});
-const openai = new OpenAIApi(configuration);
+// Configure OpenAI and Pinecone
+const openai = require("openai");
+openai.api_key = OPENAI_API_KEY;
+
+const pinecone = new Pinecone.PineconeClient();
+
+// Create Pinecone index
+const table_name = YOUR_TABLE_NAME;
+const dimension = 1536;
+const metric = "cosine";
+const pod_type = "p1";
 
 // Task list
-const taskList = [];
+const task_list = [];
 
-function addTask(task) {
-  taskList.push(task);
+function add_task(task) {
+  task_list.push(task);
 }
 
-async function createTask(prompt) {
-  const completion  = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt,
-    temperature: 0.5,
-    max_tokens: 100,
-    top_p: 1,
-  });
-  return completion.data.choices[0].text.trim().split("\n");
+function get_ada_embedding(text) {
+  text = text.replace("\n", " ");
+  return openai.Embedding.create({ input: [text], model: "text-embedding-ada-002" })["data"][0]["embedding"];
 }
 
-async function taskCreationAgent(objective, result, taskDescription, taskList) {
-  const prompt = `You are an task creation AI that uses the result of an execution agent to create new tasks with the following objective: ${objective}, The last completed task has the result: ${result}. This result was based on this task description: ${taskDescription}. These are incomplete tasks: ${taskList.join(
-    ", "
-  )}. Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks. Return the tasks as an array.`;
-  const newTasks = await createTask(prompt);
-  return newTasks.map((taskName) => ({ task_name: taskName }));
-}
-
-async function prioritizationAgent(thisTaskId) {
-  const taskNames = taskList.map((t) => t.task_name);
-  const nextTaskId = thisTaskId + 1;
-  const prompt = `You are an task prioritization AI tasked with cleaning the formatting of and reprioritizing the following tasks: ${taskNames}. Consider the ultimate objective of your team: ${OBJECTIVE}. Do not remove any tasks. Return the result as a numbered list, like:
-    #. First task
-    #. Second task
-    Start the task list with number ${nextTaskId}.`;
-  const newTasks = await createTask(prompt);
-  taskList.length = 0;
-  newTasks.forEach((taskString) => {
-    const taskParts = taskString.trim().split(".", 1);
-    if (taskParts.length === 2) {
-      const taskId = parseInt(taskParts[0].trim(), 10);
-      const taskName = taskParts[1].trim();
-      taskList.push({ task_id: taskId, task_name: taskName });
-    }
-  });
-}
-
-async function executionAgent(objective, task) {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `You are an AI who performs one task based on the following objective: ${objective}. Your task: ${task}\nResponse:`,
-    temperature: 0.7,
-    max_tokens: 2000,
-    top_p: 1,
-  });
-  return completion.data.choices[0].text.trim();
-}
-
-// Add the first task
-const firstTask = {
-  task_id: 1,
-  task_name: YOUR_FIRST_TASK,
-};
-
-addTask(firstTask);
-
-// Main loop
-async function mainLoop() {
-  let task_id_counter = 1;
-  while (true) {
-    if (taskList.length) {
-      // Print the task list
-      console.log("\x1b[95m\x1b[1m" + "\nTASK LIST\n" + "\x1b[0m\x1b[0m");
-      taskList.forEach((t) => {
-        console.log(t.task_id + ": " + t.task_name);
-      });
-
-      // Step 1: Pull the first task
-      const task = taskList.shift();
-      console.log(
-        "\x1b[92m\x1b[1m" + "\n*****NEXT TASK*****\n" + "\x1b[0m\x1b[0m"
-      );
-      console.log(task.task_id + ": " + task.task_name);
-
-      // Send to execution function to complete the task based on the context
-      const result = await executionAgent(OBJECTIVE, task.task_name);
-      const thisTaskId = task.task_id;
-      console.log(
-        "\x1b[93m\x1b[1m" + "\n*****TASK RESULT*****\n" + "\x1b[0m\x1b[0m"
-      );
-      console.log(result);
-
-      // Step 2: Enrich result and store in Pinecone (Update this section once Pinecone JavaScript library is available)
-      const enrichedResult = { data: result };
-
-      // Step 3: Create new tasks and reprioritize task list
-      const newTasks = await taskCreationAgent(
-        OBJECTIVE,
-        enrichedResult,
-        task.task_name,
-        taskList.map((t) => t.task_name)
-      );
-
-      for (const newTask of newTasks) {
-        task_id_counter += 1;
-        newTask.task_id = task_id_counter;
-        addTask(newTask);
-      }
-
-      await prioritizationAgent(thisTaskId);
-
-      // Sleep before checking the task list again
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-    } else {
-      // Sleep if the task list is empty
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-    }
+async function openai_call(prompt, use_gpt4 = false, temperature = 0.5, max_tokens = 100) {
+  if (!use_gpt4) {
+    //Call GPT-3 DaVinci model
+    const response = await openai.Completion.create({
+      engine: "text-davinci-003",
+      prompt: prompt,
+      temperature: temperature,
+      max_tokens: max_tokens,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+    return response.choices[0].text.trim();
+  } else {
+    //Call GPT-4 chat model
+    const messages = [{ role: "user", content: prompt }];
+    const response = await openai.ChatCompletion.create({
+      model: "gpt-4",
+      messages,
+      temperature,
+      max_tokens,
+      n: 1,
+      stop: null,
+    });
+    return response.choices[0].message.content.trim();
   }
 }
 
-mainLoop();
+async function task_creation_agent(objective, result, task_description, task_list, gpt_version = 'gpt-3') {
+  const prompt = `You are an task creation AI that uses the result of an execution agent to create new tasks with the following objective: ${objective}, The last completed task has the result: ${result}. This result was based on this task description: ${task_description}. These are incomplete tasks: ${task_list.join(', ')}. Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks. Return the tasks as an array.`;
+  const response = await openai_call(prompt, USE_GPT4);
+  const new_tasks = response.split('\n').map(task_name => ({task_name}));
+  return new_tasks;
+}
+
+async function prioritization_agent(this_task_id, gpt_version = 'gpt-3') {
+  const task_names = task_list.map(t => t.task_name);
+  const next_task_id = this_task_id + 1;
+  const prompt = `You are an task prioritization AI tasked with cleaning the formatting of and reprioritizing the following tasks: ${task_names.join(', ')}. Consider the ultimate objective of your team: ${OBJECTIVE}. Do not remove any tasks. Return the result as a numbered list, like:
+    1. First task
+    2. Second task
+    Start the task list with number ${next_task_id}.`;
+  const response = await openai_call(prompt, USE_GPT4);
+  const new_tasks = response.split('\n').map(task_string => {
+    const [task_id, task_name] = task_string.trim().split('.', 2).map(x => x.trim());
+    return {task_id, task_name};
+  });
+  task_list = [];
+  for (const task of new_tasks) {
+    task_list.push(task);
+  }
+}
+
+async function execution_agent(objective, task, gpt_version = 'gpt-3') {
+  const context = await context_agent(objective, YOUR_TABLE_NAME, 5);
+  const prompt = `You are an AI who performs one task based on the following objective: ${objective}.\nTake into account these previously completed tasks: ${context}\nYour task: ${task}\nResponse:`;
+  return await openai_call(prompt, USE_GPT4, 0.7, 2000);
+}
+
+async function context_agent(query, index, n) {
+  const query_embedding = await get_ada_embedding(query);
+  const indexClient = pinecone.Index(index);
+  const results = await indexClient.query(query_embedding, {
+    top_k: n,
+    include_metadata: true
+  });
+  const sorted_results = results.matches.sort((a, b) => b.score - a.score);
+  return sorted_results.map(result => result.metadata.task);
+}
+
+const first_task = {
+  task_id: 1,
+  task_name: YOUR_FIRST_TASK
+};
+add_task(first_task);
+
+let task_id_counter = 1;
+
+(async () => {
+
+  await pinecone.init({
+    apiKey: PINECONE_API_KEY,
+    environment: PINECONE_ENVIRONMENT,
+    projectName: 'Default Project'
+  });
+
+  const indexesList = await pinecone.listIndexes();
+  if (!indexesList.includes(table_name)) {
+    await pinecone.createIndex({
+      createRequest: {
+        name: table_name,
+        dimension,
+        metric,
+        podType: pod_type,
+      },
+    });
+  }
+
+  while (true) {
+    if (task_list.length > 0) {
+
+      console.log("\n*****TASK LIST*****\n");
+
+      for (const task of task_list) {
+        console.log(`${task.task_id}: ${task.task_name}`);
+      }
+
+      const task = task_list.shift();
+      console.log("\n*****NEXT TASK*****\n");
+      console.log(`${task.task_id}: ${task.task_name}`);
+
+      const result = await execution_agent(OBJECTIVE, task.task_name);
+      const this_task_id = parseInt(task.task_id);
+      console.log("\n*****TASK RESULT*****\n");
+      console.log(result);
+
+      // Step 2: Enrich result and store in Pinecone
+      const enriched_result = {'data': result};  // This is where you should enrich the result if needed
+      const result_id = `result_${task['task_id']}`;
+      const vector = enriched_result['data'];  // extract the actual result from the dictionary
+      index.upsert([
+        {
+          id: result_id,
+          values: get_ada_embedding(vector),
+          metadata: {
+            task: task['task_name'],
+            result: result
+          }
+        }
+      ]);
+
+      // Step 3: Create new tasks and reprioritize task list
+      const new_tasks = task_creation_agent(OBJECTIVE, enriched_result, task["task_name"], task_list.map(t => t.task_name));
+
+      for (const new_task of new_tasks) {
+          task_id_counter++;
+          new_task.task_id = task_id_counter;
+          add_task(new_task);
+      }
+      prioritization_agent(this_task_id);
+
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Sleep before checking the task list again
+    }
+  }
+})()
